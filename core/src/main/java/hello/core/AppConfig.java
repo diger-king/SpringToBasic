d17bb6a5e69324f2ae35633d88bc639d8c42bdb3
Diger-king <peter6081@gmail.com>
2022-01-28 18:36:01 +0900

    스프링 컨테이너, 빈 조회 실습 1차

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,23 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구현영역 -> AppConfig
 // 사용영역 -> ~~~Impl
+
+@Configuration
 public class AppConfig { //AppConfig 는 공연 기획자
 
+    @Bean
     public MemberService memberService() { //MemberService는 공연 배우1
         return new MemberServiceImpl(memberRepository()); // 공연 배우가 필요한 장비를 공연 기획자가 지시해준것
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
+    }
+
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
